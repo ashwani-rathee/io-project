3ffa109eb2207471c00e3a5d5bcf2ba1f3895db7
Ashwani Rathee <ab669522@gmail.com>
2022-04-06 13:29:14 +0530

    libjpeg2ktest update

--- jpeg2000wrapper/libjpeg2ktest.jl
+++ jpeg2000wrapper/libjpeg2ktest.jl
@@ -3,7 +3,20 @@ include("lib/LibOpenJpeg.jl")
 push!(DL_LOAD_PATH, "./libopenjpegextra")
 
 filename = "./libopenjpegextra/notes/sample1.jp2"
+println("\n")
+n = ccall((:decode, :libopenjpegextra), Ptr{LibOpenJpeg.opj_image_t}, (Ptr{Cchar},), filename)
+println("\n")
 
-n = ccall((:decode, :libopenjpegextra), LibOpenJpeg.opj_image_t, (Ptr{Cchar},), filename)
+N = unsafe_load(n)
+# print(N)
+println(unsafe_load(unsafe_load(N.comps).data))
+# load a image from filename:
+# support different versions of colorspaces
+# save in different versions
 
-print(typeof(n.numcomps))
+function load(filename::String)
+    filename = "./libopenjpegextra/notes/sample1.jp2"
+    n = ccall((:decode, :libopenjpegextra), Ptr{LibOpenJpeg.opj_image_t}, (Ptr{Cchar},), filename)
+    N = unsafe_load(n)
+    println(N.numcomps)
+end
