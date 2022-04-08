1fa7790faeac05161af0e1b7c1c96ea51f8aab1c
Ashwani Rathee <ab669522@gmail.com>
2022-04-08 12:13:44 +0530

    load function for jpeg 2000

--- jpeg2000wrapper/libjpeg2ktest.jl
+++ jpeg2000wrapper/libjpeg2ktest.jl
@@ -1,22 +1,36 @@
+##
+# Run this file as: julia --project=. libjpeg2ktest.jl inside (base) ashwani@user:~/Desktop/GSOC/io-project/jpeg2000wrapper$
+##
+## imports
 using Libdl
+using FileIO
+using ColorTypes
+using ColorVectorSpace
+using FixedPointNumbers
+
 include("lib/LibOpenJpeg.jl")
 push!(DL_LOAD_PATH, "./libopenjpegextra")
 
-filename = "./libopenjpegextra/notes/sample1.jp2"
-println("\n")
-n = ccall((:decode, :libopenjpegextra), Ptr{LibOpenJpeg.opj_image_t}, (Ptr{Cchar},), filename)
-println("\n")
 
-N = unsafe_load(n)
-# print(N)
-println(unsafe_load(unsafe_load(N.comps).data))
 # load a image from filename:
 # support different versions of colorspaces
 # save in different versions
 
-function load(filename::String)
-    filename = "./libopenjpegextra/notes/sample1.jp2"
+RGB = 1
+GRAY = 0
+
+function load(filename::AbstractString;)
     n = ccall((:decode, :libopenjpegextra), Ptr{LibOpenJpeg.opj_image_t}, (Ptr{Cchar},), filename)
     N = unsafe_load(n)
-    println(N.numcomps)
+    components = unsafe_wrap(Array, N.comps, N.numcomps)
+    println("Width:", N.x1,", Height:", N.y1)
+    data = convert(Array{UInt8}, (unsafe_wrap(Array, components[1].data, N.x1 * N.y1)))
+    res = reinterpret(Gray{N0f8}, data)
+    res = reshape(res, Int64(N.x1), Int64(N.y1))
+    res =  res'
+    # save("./test.jpg",res)
+    return res
 end
+
+filename = "./libopenjpegextra/notes/sample1.jp2"
+load(filename)
